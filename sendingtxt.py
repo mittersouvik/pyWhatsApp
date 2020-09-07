edda981d80cdb5df6dec7c9bc6b14c7a5d3c095b
Souvik Mitra <mittersouvik@gmail.com>
2020-09-08 01:19:39 +0530

    change the number

--- sendingtxt.py
+++ sendingtxt.py
@@ -2,4 +2,4 @@ import pywhatkit as kit
 
 #kit.add_driver_path(r'C:\Users\Souvik Mitra\Downloads\ChromeSetup.exe')
 #kit.load_QRcode()
-kit.sendwhatmsg("+918272948145","Hi Welcome to PyMitter",00,53)
+kit.sendwhatmsg("+91XXXXXXXXXX","Hi Welcome to PyMitter",00,53)

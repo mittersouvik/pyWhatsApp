2f4d80b277f0a1e2536a43d12d434d263656dcad
Souvik Mitra <mittersouvik@gmail.com>
2020-09-08 01:14:38 +0530

    Send text msgs to a number

---
+++ sendingtxt.py
@@ -0,0 +1,5 @@
+import pywhatkit as kit
+
+#kit.add_driver_path(r'C:\Users\Souvik Mitra\Downloads\ChromeSetup.exe')
+#kit.load_QRcode()
+kit.sendwhatmsg("+918272948145","Hi Welcome to PyMitter",00,53)
